e99e7e7f6a99901b21ea3ddab85bcde50087de8c
PranjalSharma22 <pranjal.alwar@gmail.com>
2018-09-21 10:42:09 +0530

    reading data from html and writing to excel file

--- FORM/__init__.py
+++ FORM/__init__.py
@@ -1,25 +1,33 @@
 from flask import Flask,render_template,request
 import pandas as pd
+from pathlib import Path
 app = Flask(__name__)
-df=pd.DataFrame(columns=['First Name','Last Name'])
+import xlsxwriter
 @app.route('/',methods=['GET','POST'])
 def homepage():	
 	
 	if request.method =='POST':
-		writer = pd.ExcelWriter('pandas_simple11.xlsx', engine='openpyxl')
-		first_name=request.form['first_name']
-		last_name=request.form['last_name']
-		print ('Hello' + " " + first_name + " " + last_name )
-		global df
-		data = [{'First Name': first_name, 'Last Name': last_name}]
-		df1=pd.DataFrame(data,columns=['First Name','Last Name'])
-		df=df.append(df1)
-		df.index = range(1,len(df)+1)
-		df.to_excel(writer, sheet_name='Sheet1')
-	#	df.to_excel(writer, startrow=len(df)+1)
-		writer.save()
+		config = Path('./pandas_simple13.xlsx') 
+		if config.is_file(): 
+			add()
+		else:	
+			# Create an new Excel file and add a worksheet.
+			workbook = xlsxwriter.Workbook('pandas_simple13.xlsx')
+			workbook.close()
+			add()
+
 	return render_template("index.html")
+def add():
+	first_name=request.form['first_name']
+	last_name=request.form['last_name']
+	print ("hello")	
+	data = [{'First Name': first_name, 'Last Name': last_name}]
+	df1=pd.DataFrame(data,columns=['First Name','Last Name'])	
+	ex=pd.read_excel('pandas_simple13.xlsx')
+	ex=ex.append(df1)
+	ex.index = range(1,len(ex)+1)
+	ex.to_excel('pandas_simple13.xlsx')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
